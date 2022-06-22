8e9371ee6b18bbabf9d09cbb7fb771fb2a287b60
del-alj <douae.elalj@gmail.com>
2022-06-22 21:31:46 +0100

    fix grid size

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -9,6 +9,7 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: "center",
   color: theme.palette.text.secondary,
+  borderRadius: "20px",
 }));
 
 export const Home = () => {
@@ -18,17 +19,17 @@ export const Home = () => {
       // style={{ border: "1px solid black" }}
       >
         ok
-        <Grid container spacing={2}>
-          <Grid style={{  minHeight:"400px" }} item xs={6} md={6}>
+        <Grid style={{ justifyContent: "center"  }} container spacing={2}>
+          <Grid style={{  minHeight:"400px" }} item xs={10} md={5}>
             {/* <Box sx={{ backgroundColor: "blue", height: "100px" }}> */}
             <Title size="3em">{home.BigTitle}</Title>
             <Title size="1em">{home.text}</Title>
             {/* </Box> */}
           </Grid>
-          <Grid item xs={6} md={6}>
+          <Grid item xs={10} md={5}>
             <Item  style={{  minHeight:"400px" }}>xs=6 md=4</Item>
           </Grid>
-          <Grid item xs={6} md={12}>
+          <Grid item xs={10} md={10}>
             <Item style={{background: "#659999",  
                 background: "-webkit-linear-gradient(to bottom, #f4791f, #659999)",  
             background: "linear-gradient(to bottom, #f4791f, #659999)"
@@ -36,7 +37,7 @@ export const Home = () => {
               <div>xs=6 md=4</div>
             </Item>
           </Grid>
-          <Grid item xs={6} md={12}>
+          <Grid item xs={10} md={10}>
             <Item style={{background: "rgb(172,180,179)",
 background: "linear-gradient(180deg, rgba(172,180,179,1) 0%, rgba(174,140,104,1) 100%)", minHeight:"500px" }}>xs=6 md=8</Item>
           </Grid>
