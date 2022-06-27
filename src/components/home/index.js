20f9725208a8e9c8befbedd42c5601d0892ced6a
del-alj <douae.elalj@gmail.com>
2022-06-27 23:54:06 +0100

    add images

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { Box, Paper, Grid, styled } from "@mui/material";
 import { home } from "./paragraph";
-import { Title } from "./style";
-import {backgrounds} from "./backgroundColors.js"
+import { Title, Card } from "./style";
+import { backgrounds } from "./backgroundColors.js";
+import tetris from "../../assets/redtetris.png";
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -11,41 +12,47 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
   borderRadius: "20px",
   "&:hover": {
-    boxShadow: "rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px",
-    }
+    boxShadow:
+      "rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px"
+  }
 }));
-
-
+const projects = home.projects;
 export const Home = () => {
   return (
-    <Box sx={{ flexGrow: 1}}>
-      <div 
+    <Box sx={{ flexGrow: 1 }}>
+      <div
       // style={{ border: "1px solid black" }}
       >
-        <Grid style={{ justifyContent: "center"  }} container spacing={15}>
-          <Grid style={{  minHeight:"400px" }} item xs={10} md={5}>
+        <Grid style={{ justifyContent: "center" }} container spacing={15}>
+          <Grid style={{ minHeight: "400px" }} item xs={10} md={5}>
             {/* <Box sx={{ backgroundColor: "blue", height: "100px" }}> */}
             <Title size="3em">{home.BigTitle}</Title>
             <Title size="1em">{home.text}</Title>
             {/* </Box> */}
           </Grid>
           <Grid item xs={10} md={5}>
-            <Item  style={{  minHeight:"400px" }}>xs=6 md=4</Item>
+            <Item style={{ minHeight: "400px" }}>xs=6 md=4</Item>
           </Grid>
-          <Grid item xs={10} md={10}>
-            <Item style={{
-            background: backgrounds[0].background, 
-            background: backgrounds[0].linearGradient,
-            minHeight:"1000px" }}>
-              <div>xs=6 md=4</div>
+          {projects.map((project, index) => { return (<Grid item xs={10} md={5}>
+            <Item
+              style={{
+                background: backgrounds[0].background,
+                background: backgrounds[0].linearGradient,
+                minHeight: "700px",
+                justifyContent: "center"
+              }}
+            >
+              <Card>
+                <h1>{project.title}</h1>
+                <div>
+                  <img src={project.picture} />
+                </div>
+              </Card>
             </Item>
-          </Grid>
-          <Grid item xs={10} md={10}>
-            <Item style={{
-            background: backgrounds[1].background, 
-            background: backgrounds[1].linearGradient,
-            minHeight:"1000px" }}>xs=6 md=8</Item>
-          </Grid>
+          </Grid>)
+
+})}
+         
         </Grid>
       </div>
     </Box>
--- src/components/home/style.js
+++ src/components/home/style.js
@@ -1,6 +1,27 @@
 import styled from "styled-components";
-
 export const Title = styled.h1`
-font-size: ${(props) => (props.size ? props.size : "12px")};
+  font-size: ${(props) => (props.size ? props.size : "12px")};
+`;
 
+export const Card = styled.div`
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  border: "1px solid red";
+  > div {
+    border: "1px solid black";
+    height: auto;
+    justify-content: center;
+    align-items: center;
+  }
+  > div > img {
+    :hover {
+      border: 1px solid black;
+      box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;
+    }
+    width: 90%;
+    height: auto;
+  }
 `;
