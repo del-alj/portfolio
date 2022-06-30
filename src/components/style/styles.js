088bf127251462edc50061e2ac2075f4bd061b27
del-alj <douae.elalj@gmail.com>
2022-06-30 23:32:53 +0100

    fix header

--- src/components/style/styles.js
+++ src/components/style/styles.js
@@ -4,5 +4,4 @@ export const ContainerDiv = styled(Container)(({ theme }) => ({
   // backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(20),
-  // background: "red"
 }));
