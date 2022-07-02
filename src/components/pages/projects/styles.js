575855ab7dc2318fd51650c81bf31d4b124c4807
del-alj <douae.elalj@gmail.com>
2022-07-02 02:24:23 +0100

    fix width project

--- src/components/pages/projects/styles.js
+++ src/components/pages/projects/styles.js
@@ -11,7 +11,7 @@ export const DivGrid = styled.div`
 export const DivItem = styled.div`
   @media (max-width: 470px) {
     > div {
-      width: 30em;
+      /* width: 30em; */
       font-size: 20px;
     }
   }
